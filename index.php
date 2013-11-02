c62d8a178f423b9983b4c98e2a10239623e2ec09
Henry Auffahrt <henry2@auffie.de>
2013-11-02 17:56:18 +0100

    testweise mal was veraendert

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 	echo "Hello World <br><br>";
-	echo "5 + 5 = ". 5+5;
-
+	echo "5 + 5 = ". 5+5 . "(da m&uuml;sste jetzt 10 stehen.) <br>";
+	echo "Ich glaube jetzt funktioniert es! :)";
 ?>
 	
